64fbe1e7b30bd07e4e61b11cf98292f1cc81bfd4
Vinicius Ortiz <vinicius.oaw@gmail.com>
2022-11-23 12:31:00 -0300

    feat: add portuguese translation

--- core/test/locale.test.js
+++ core/test/locale.test.js
@@ -35,3 +35,39 @@ test('test render', () => {
     }
   })).toBe('1-2')
 })
+
+test('test getLocaleStr pt', () => {
+  const l = getLocale('pt', {
+    custom: {
+      '*': 'bar',
+      message: 'baz'
+    }
+  })
+
+  expect(l.getLocaleStr('year', 'minute', 'empty', 'text')).toBe('cada minuto')
+  expect(l.getLocaleStr('year', 'dayOfWeek', 'value', 'prefix')).toBe('e de')
+  expect(l.getLocaleStr('year', 'minute', 'range', 'prefix')).toBe(':')
+  expect(l.getLocaleStr('custom', 'foo')).toBe('bar')
+  expect(l.getLocaleStr('custom', 'message')).toBe('baz')
+})
+
+test('test render pt', () => {
+  const l = getLocale('pt', {
+    '*': {
+      '*': {
+        '*': {
+          '*': '{{start.text}}-{{end.text}}'
+        }
+      }
+    }
+  })
+
+  expect(l.render('period', 'field', 'type', 'pos', {
+    start: {
+      text: '1'
+    },
+    end: {
+      text: '2'
+    }
+  })).toBe('1-2')
+})
