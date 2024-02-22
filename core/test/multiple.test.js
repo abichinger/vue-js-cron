65247dc4bb21a281650c3eab79c02c5e757f25ff
brandon.arino@carvana.com <brandon.arino@carvana.com>
2024-02-22 15:44:16 -0600

    fix(core): move every-at tests to their own tests.

--- core/test/multiple.test.js
+++ core/test/multiple.test.js
@@ -25,11 +25,6 @@ test('test strToArray', () => {
     expect(multiple.strToArray('100', r(0, 59))).toBe(null);
     expect(multiple.strToArray('0', r(1, 10))).toBe(null);
     expect(multiple.strToArray('*/90', r(1, 10))).toBe(null);
-
-    expect(multiple.strToArray('5/10', r(0, 59))).toEqual([5, 15, 25, 35, 45, 55]);
-    expect(multiple.strToArray('5/10', r(8, 59))).toEqual([15, 25, 35, 45, 55]);
-    expect(multiple.strToArray('3/8', r(7, 59))).toEqual([11, 19, 27, 35, 43, 51, 59]);
-
 });
 
 test('test arrayToStr', () => {
@@ -43,7 +38,18 @@ test('test arrayToStr', () => {
     expect(multiple.arrayToStr([7,14,21,28], r(5, 30)).value).toEqual('*/7');
     expect(multiple.arrayToStr([0,5,10], r(0, 20)).value).toEqual('0,5,10');
     expect(multiple.arrayToStr([1,2,5,8,9,10], r(1, 10)).value).toEqual('1-2,5,8-10');
+});
 
+test('test every-at strToArray', () => {
+    let multiple = new MultipleColumns(true)
+    expect(multiple.strToArray('5/10', r(0, 59))).toEqual([5, 15, 25, 35, 45, 55]);
+    expect(multiple.strToArray('5/10', r(8, 59))).toEqual([15, 25, 35, 45, 55]);
+    expect(multiple.strToArray('3/8', r(7, 59))).toEqual([11, 19, 27, 35, 43, 51, 59]);
+
+});
+
+test('test every-at arrayToStr', () => {
+    let multiple = new MultipleColumns(true)
     expect(multiple.arrayToStr([5, 15, 25, 35, 45, 55], r(0, 59)).value).toEqual('5/10');
     expect(multiple.arrayToStr([15, 25, 35, 45, 55], r(10, 59)).value).toEqual('15/10');
 });
