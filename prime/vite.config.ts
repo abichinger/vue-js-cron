68a60e5cbe8676479dd7503195c6af6da9249c36
Andreas Bichinger <andreas.bichinger@gmail.com>
2024-11-11 13:18:58 +0100

    feat: update vite configuration

--- prime/vite.config.ts
+++ prime/vite.config.ts
@@ -1,18 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      rollupTypes: true,
+      entryRoot: 'src',
+    }),
   ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'CronPrime',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['vue', '@vue', 'primevue', '@primevue/themes'],
+      output: {
+        assetFileNames: 'prime.css',
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+          '@vue-js-cron/core': 'CronCore',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
