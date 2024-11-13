230cdcb0fcd82568fbd2e1f6dce252e8849c2580
Andreas Bichinger <andreas.bichinger@gmail.com>
2024-11-13 18:13:03 +0100

    feat: add Noir preset

--- prime/src/main.ts
+++ prime/src/main.ts
@@ -1,7 +1,19 @@
+import { Noir } from '@/presets/Noir'
+import 'primeicons/primeicons.css'
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: Noir,
+    options: {
+      prefix: 'p',
+      darkModeSelector: '.p-dark',
+      cssLayer: false,
+    },
+  },
+})
+
 app.mount('#app')
