a2ce2b781cf51e949fdebc398ea1443d1056c722
Lars Sommer <ls@iot-sgdd.com>
2024-11-14 14:01:46 +0100

    fix: update da.ts #67

--- core/src/locale/da.ts
+++ core/src/locale/da.ts
@@ -6,33 +6,53 @@ const locale: Localization = {
     suffix: '',
     text: 'Ukendt',
     '*': {
-      empty: { text: 'hver {{field.id}}' },
       value: { text: '{{value.text}}' },
       range: { text: '{{start.text}}-{{end.text}}' },
       everyX: { text: 'hver {{every.value}}' },
     },
     month: {
       '*': { prefix: 'i' },
+      empty: { text: 'hver måned' },
       value: { text: '{{value.alt}}' },
       range: { text: '{{start.alt}}-{{end.alt}}' },
     },
     day: {
-      '*': { prefix: 'på' },
+      '*': { prefix: 'og' },
+      empty: { text: 'hver dag' },
+      value: { text: '{{value.alt}}.' },
+      range: { text: '{{start.alt}}.-{{end.alt}}.' },
+      noSpecific: {
+        text: 'ingen specifik dag',
+      },
     },
+
     dayOfWeek: {
       '*': { prefix: 'på' },
       empty: { text: 'hver dag i ugen' },
       value: { text: '{{value.alt}}' },
       range: { text: '{{start.alt}}-{{end.alt}}' },
+      noSpecific: {
+        text: 'ingen specifik dag i ugen',
+      },
     },
     hour: {
       '*': { prefix: 'klokken' },
+      empty: { text: 'hver time' },
     },
     minute: {
       '*': { prefix: ':' },
+      empty: { text: 'hvert minut' },
+    },
+    second: {
+      '*': { prefix: ':' },
+      empty: { text: 'hvert sekund' },
     },
   },
+  second: {
+    prefix: 'Hvert',
+  },
   minute: {
+    prefix: 'Hvert',
     text: 'Minut',
   },
   hour: {
@@ -58,9 +78,32 @@ const locale: Localization = {
     },
   },
   year: {
+    prefix: 'Hvert',
     text: 'År',
     dayOfWeek: {
       '*': { prefix: 'og' },
+    },
+  },
+
+  // quartz format
+  'q-second': {
+    text: 'Sekund',
+  },
+  'q-minute': {
+    text: 'Minut',
+    second: {
+      '*': {
+        suffix: 'sekund(er)',
+      },
+      empty: { text: 'hvert' },
+    },
+  },
+  'q-hour': {
+    text: 'Time',
+    minute: {
+      '*': {
+        prefix: 'på',
+      },
     },
   },
 }
