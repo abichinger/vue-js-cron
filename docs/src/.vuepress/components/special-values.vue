c34a91766aa5629a4085efb987ab21cc065021df
Ismoil <braboev@gmail.com>
2026-09-04 17:56:57 +0500

    docs: add guide for special values
    Documents `L`, `L-<n>`, `LW` and `<n>W` of the day of month field and
    shows how to enable them for the crontab format.

---
+++ docs/src/.vuepress/components/special-values.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="mb-2">{{ value }}</div>
+    <CronLight v-model="value" format="quartz" @error="error = $event" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '0 0 0 L * ?',
+      error: '',
+    }
+  },
+}
+</script>
